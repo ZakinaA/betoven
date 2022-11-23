b2b0612562b0b89b9dc2d164c1f93c3966a9e9b3
JLE.Git <55048956+JLEGit@users.noreply.github.com>
2022-11-23 07:39:18 +0100

    Mise à jour des entités

--- src/Entity/Paiement.php
+++ src/Entity/Paiement.php
@@ -2,21 +2,17 @@
 
 namespace App\Entity;
 
-use App\Repository\PaiementRepository;
+use App\Repository\PAIEMENTRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: PaiementRepository::class)]
-class Paiement
+#[ORM\Entity(repositoryClass: PAIEMENTRepository::class)]
+class PAIEMENT
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
-
-    #[ORM\ManyToOne]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?Inscription $inscription = null;
 
     #[ORM\Column(length: 10)]
     private ?string $montant = null;
@@ -24,21 +20,13 @@ class Paiement
     #[ORM\Column(type: Types::DATE_MUTABLE)]
     private ?\DateTimeInterface $datePaiement = null;
 
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?inscription $paiementInscription = null;
+
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getInscription(): ?Inscription
-    {
-        return $this->inscription;
-    }
-
-    public function setInscription(?Inscription $inscription): self
-    {
-        $this->inscription = $inscription;
-
-        return $this;
     }
 
     public function getMontant(): ?string
@@ -61,6 +49,18 @@ class Paiement
     public function setDatePaiement(\DateTimeInterface $datePaiement): self
     {
         $this->datePaiement = $datePaiement;
+
+        return $this;
+    }
+
+    public function getPaiementInscription(): ?inscription
+    {
+        return $this->paiementInscription;
+    }
+
+    public function setPaiementInscription(?inscription $paiementInscription): self
+    {
+        $this->paiementInscription = $paiementInscription;
 
         return $this;
     }
