26b9cf48248b68e0cfb0c1cd4dfee060d0ab9aed
JLE.Git <55048956+JLEGit@users.noreply.github.com>
2022-11-28 10:45:05 +0100

    Add files via upload

---
+++ src/Controller/InstrumentController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Instrument;
+use Doctrine\Persistence\ManagerRegistry;
+
+class InstrumentController extends AbstractController
+{
+    #[Route('/instrument', name: 'app_instrument')]
+    public function index(): Response
+    {
+        return $this->render('instrument/index.html.twig', [
+            'controller_name' => 'InstrumentController',
+        ]);
+    }
+
+    public function ajouterInstrument(ManagerRegistry $doctrine){
+		
+        // récupère le manager d'entités
+            $entityManager = $doctrine->getManager();
+    
+            // instanciation d'un objet Instrument
+            $instrument = new Instrument();
+            $instrument->setNumSerie('28M4GF');
+            $instrument->setDateAchat(new \DateTime(date('1999-07-14')));
+            $instrument->setPrixAchat(25);
+    
+            // Indique à Doctrine de persister l'objet
+            $entityManager->persist($instrument);
+    
+            // Exécue l'instruction sql permettant de persister lobjet, ici un INSERT INTO
+            $entityManager->flush();
+    
+            // renvoie vers la vue de consultation de l'étudiant en passant l'objet instrument en paramètre
+           return $this->render('instrument/consulter.html.twig', [
+                'instrument' => $instrument,]);
+            
+        }
+
+        public function consulterInstrument(ManagerRegistry $doctrine, int $id){
+
+            $instrument= $doctrine->getRepository(Instrument::class)->find($id);
+            
+            if (!$instrument) {
+                throw $this->createNotFoundException(
+                'Aucun instrument trouvé avec le numéro '.$id
+                );
+            }
+            //return new Response('Instrument : '.$instrument->getNom());
+            return $this->render('instrument/consulter.html.twig', [
+                'instrument' => $instrument,]);
+        }
+
+        public function listerInstrument(ManagerRegistry $doctrine){
+
+            $repository = $doctrine->getRepository(Instrument::class);
+    
+    $instruments= $repository->findAll();
+    return $this->render('instrument/lister.html.twig', [
+        'pInstruments' => $instruments,]);	
+        
+}
+}
