1b157db9ac116f939e61103cf6997a67d6ccc71c
BenjaminL6 <benjamin.loiseau62@gmail.com>
2022-12-05 11:00:20 +0100

    Fix

--- src/Entity/Paiement.php
+++ src/Entity/Paiement.php
@@ -2,11 +2,11 @@
 
 namespace App\Entity;
 
-use App\Repository\PaiementRepository;
+use App\Repository\PAIMENTRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: PaiementRepository::class)]
+#[ORM\Entity(repositoryClass: PAIMENTRepository::class)]
 class PAIEMENT
 {
     #[ORM\Id]
